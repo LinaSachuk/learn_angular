2fb2c06d51468811429a205aa0f398cac787300b
Sachuk <lsachuk@igskcicgltgm025.gs.doi.net>
2021-02-12 17:47:54 -0700

    add angular material

---
+++ learn-app/src/app/basic-form/basic-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-basic-form',
+  templateUrl: './basic-form.component.html',
+  styleUrls: ['./basic-form.component.css'],
+})
+export class BasicFormComponent implements OnInit {
+  myForm: FormGroup;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
