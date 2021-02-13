b1e83aea2f3684db6b700e4c6fe0ff48d61eb683
Sachuk <lsachuk@igskcicgltgm025.gs.doi.net>
2021-02-12 18:40:23 -0700

    add basic form

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # learn_angular
+
+https://www.youtube.com/watch?v=JeeUY6WaXiA
--- learn-app/src/app/basic-form/basic-form.component.ts
+++ learn-app/src/app/basic-form/basic-form.component.ts
@@ -9,7 +9,24 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class BasicFormComponent implements OnInit {
   myForm: FormGroup;
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
+  teams: any[] = [
+    { name: 'Liverpool' },
+    { name: 'Manchester City' },
+    { name: 'Manchester United' },
+    { name: 'Arsenal' },
+    { name: 'Leicester City' },
+    { name: 'Chelsea' },
+    { name: 'Tottenham Hotspur' },
+  ];
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      email: '',
+      message: '',
+      teams: [],
+    });
+
+    this.myForm.valueChanges.subscribe(console.log);
+  }
 }
